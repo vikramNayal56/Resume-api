ced93468a3f7b9ac593a733e1303357d3fd41526
vikramNayal56 <nayalvikram049@gmail.com>
2026-07-20 04:14:03 -0700

    modify backup files

--- backup/routes/controllers/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// connect document routes
-
-router.use("/documents",require("./documentRoutes"));
-
-module.exports =  router;
